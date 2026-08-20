fefb7e056fb5eade572972b8be9f875977119ad5
Daw <da.daw648@gmail.com>
2026-08-20 15:58:33 -0400

    cpu pack

--- cpu_sv_package.sv
+++ cpu_sv_package.sv
@@ -22,4 +22,18 @@ package cpu_sv_package;
     OP_B_IMM = 1'b1
   } sel_alu_b_e;
 
+  // ALU operation select
+  typedef enum logic [3:0] {
+    ALU_ADD  = 4'd0,
+    ALU_SUB  = 4'd1,
+    ALU_SLT  = 4'd2,
+    ALU_SLTU = 4'd3,
+    ALU_SLL  = 4'd4,
+    ALU_XOR  = 4'd5,
+    ALU_SRL  = 4'd6,
+    ALU_SRA  = 4'd7,
+    ALU_OR   = 4'd8,
+    ALU_AND  = 4'd9
+  } alu_op_e;
+
 endpackage
